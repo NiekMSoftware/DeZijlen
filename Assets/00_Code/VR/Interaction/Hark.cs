2cbbfed5b41edff920d8139fa4c6fea9a654509c
Walter van der Broek <wbvanderbroek@st.noorderpoort.nl>
2025-03-21 13:57:06 +0100

    Chore: Created a rake

---
+++ Assets/00_Code/VR/Interaction/Hark.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace DeZijlen.VR
+{
+    public class Hark : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
